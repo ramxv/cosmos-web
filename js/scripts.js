e3dafc8862bfe96f2f66d300619d13e94c9b8feb
Ram Singh <ramxv.singh@gmail.com>
2024-06-21 14:53:23 -0500

    style: cambiar fondo del index.html, cambiar estilo del navbar

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,27 @@ function cargarPagina(page) {
     })
     .catch((error) => console.error("Error al cargar la página:", error));
 }
+
+function crearEstrellas() {
+	const container = document.querySelector("body");
+	for (let i = 0; i < 1000; i++) {
+			const estrella = document.createElement("div");
+			estrella.className = "estrella";
+			
+			// Posición inicial aleatoria
+			const x = Math.random() * 100;
+			const y = Math.random() * 100;
+			
+			estrella.style.top = `${y}%`;
+			estrella.style.left = `${x}%`;
+
+			// Variación de la duración de la animación para cada estrella
+			const duration = (Math.random() * 30) + 20; // Entre 20 y 50 segundos
+			estrella.style.animationDuration = `${duration}s`;
+
+			container.appendChild(estrella);
+	}
+}
+
+crearEstrellas();
+
